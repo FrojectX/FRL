c4ed4ca353cca58ebc8c78bbccac528f880970a4
Виктор <29869149+ViktorMorgachev@users.noreply.github.com>
2018-06-17 23:54:34 +0600

    Realized Menu Module, tommorow

---
+++ app/src/main/java/com/projectx/frl/menu/MenuActivity.java
@@ -0,0 +1,35 @@
+package com.projectx.frl.menu;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+
+import com.projectx.frl.R;
+import com.projectx.frl.menu.fragment.MenuFragment;
+
+public class MenuActivity extends AppCompatActivity {
+
+    private FragmentManager mFragmentManager = getSupportFragmentManager();
+    private Fragment mFragment;
+    //private final ModuleInteractor mModuleInteractor = new ModuleInteractor();
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.simple_fragment_container);
+
+        mFragment = mFragmentManager.findFragmentById(R.id.fragment_container);
+        if (mFragment == null) {
+            mFragment = new MenuFragment();
+            mFragmentManager.beginTransaction()
+                    .add(R.id.fragment_container, mFragment)
+                    .commit();
+        }
+
+
+    }
+
+}
